de7f1f29b3e498123afde50f25992a69ff0dabfb
Alex Incerti <alex.incerti@outlook.com>
2022-05-23 06:27:52 +0200

    title

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
       <header className="App-header">
         <div className='div-logo'>
           <img src={logo} className="App-logo" alt="logo" />
+          <div style={{paddingTop: "10px", paddingLeft: "16px"}}>
+            Proposal <a style={{color:'#1876d1'}} href='https://station.terra.money/proposal/1623'>#1623</a> - Rebirth Terra Network
+          </div>
         </div>
         <PoolChart/>
       </header>
